f80bef1b36ae08eb1de2951003655bb9cc1caf8f
Sam Bloomberg <sam@redxdev.com>
2014-03-31 22:21:40 -0400

    removed old tilesets from who-knows-when

--- Dist/content/tilesets/16x24rpgdb.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="16x24rpgdb" tilewidth="32" tileheight="48">
- <image source="../textures/characters/16x24rpgdb.gif" width="256" height="288"/>
-</tileset>
--- Dist/content/tilesets/GRS2ROC.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="GRS2ROC" tilewidth="40" tileheight="40">
- <image source="../textures/tilesets/2DCircle/GRS2ROC.bmp" trans="4b637f" width="640" height="480"/>
-</tileset>
