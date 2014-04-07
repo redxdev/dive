cfa2ca9290b9540cba043e563ba11eb490cd6f41
Sam Bloomberg <sam@redxdev.com>
2014-04-07 12:40:52 -0400

    added template spatial

---
+++ Engine/Engine/Templates/SpatialTemplate.cs
@@ -0,0 +1,22 @@
+﻿namespace Dive.Engine.Templates
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Dive.Engine.Components;
+    using Dive.Entity;
+    using Dive.Entity.Attributes;
+
+    [EntityTemplate(Name = "Engine.Spatial")]
+    public class SpatialTemplate : ITemplate
+    {
+        public Entity BuildEntity(Entity entity, params object[] args)
+        {
+            entity.AddComponent<TransformComponent>();
+
+            return entity;
+        }
+    }
+}
