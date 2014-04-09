2270390b997212b00198de557001d1b1ba024a8b
Sam Bloomberg <sam@redxdev.com>
2014-04-09 16:44:40 -0400

    better script error messages, closing #10

---
+++ Engine/Script/ParseException.cs
@@ -0,0 +1,33 @@
+﻿namespace Dive.Script
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Exception for parse/syntax errors.
+    /// </summary>
+    public class ParseException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ParseException"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public ParseException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ParseException"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="inner">The inner.</param>
+        public ParseException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
